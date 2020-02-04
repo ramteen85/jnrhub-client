220365698d3addee9c528d03ef5004c4ef7855d7
Ramteen Taheri <ramteen.taheri@gmail.com>
2020-02-04 16:11:44 +1100

    working on login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,12 +3,41 @@ import styles from "./Login.module.css";
 
 class Login extends Component {
 
+    state = {
+        username: '',
+        password: ''
+    }
+
+    loginHandler = (e) => {
+        e.preventDefault();
+        console.log("test");
+        console.log("event: ");
+        console.log(e);
+        console.log("state: ");
+        console.log(this.state);
+
+        //TODO: 
+        // clear input fields when submitting
+        // grab login info and redirect to landing pages accordingly
+    }
+
+    inputChangeHandler = (event) => {
+        event.preventDefault();
+        const value = event.target.value;
+        this.setState({
+            ...this.state,
+            [event.target.name]: value
+        });
+    }
 
     render() {
         return(
             <div className={styles.container}>
-                <input type="text" placeholder="Username.."/>
-                <input type="password" placeholder="Password.."/>
+                <form onSubmit={this.loginHandler}>
+                    <input type="text" name="username" onChange={this.inputChangeHandler} placeholder="Username.."/>
+                    <input type="password" name="password" onChange={this.inputChangeHandler} placeholder="Password.."/>
+                    <button type="submit">Login</button>
+                </form>
             </div>
         );
     }
