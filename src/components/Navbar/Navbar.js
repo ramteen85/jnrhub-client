530bf93fb4d155ac8beb3a238db2029f512f4153
Ramteen Taheri <ramteen.taheri@gmail.com>
2020-02-05 15:31:25 +1100

    fixed collision

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,41 +5,22 @@ import {Link, withRouter} from 'react-router-dom';
 class Navbar extends Component {
 
     state = {
-        loggedin: false
     }
 
     constructor(props) {
         super(props);
-        this.state.loggedin = this.props.loggedin;
-    }
-
-    logoutHandler = () => {
-        this.loggedin=false
-        this.props.history.push('/');
     }
 
     render() {
         return(
-<<<<<<< HEAD
             <div className={styles.navwrapper}>
-                { this.state.loggedin
-                ?
                 <nav className={styles.loggedinnav}>
                     <p className={styles.brand}>&lt;Jnr/Hub&gt;</p>
                     <ul>
-<<<<<<< HEAD
                         <li><a href="/">Dashboard</a></li>
                         <li><a href="/">Job Board</a></li>
                         <li><a href="/">Profile</a></li>
                         <li><a href="/">Logout</a></li>
-                    </ul>
-                </nav>
-                :
-                <nav>
-                    <ul>
-                        <li>
-
-                        </li>
                     </ul>
                 </nav>
                 }
@@ -49,8 +30,4 @@ class Navbar extends Component {
 
 }
 
-<<<<<<< HEAD
-export default Navbar;
-=======
 export default withRouter(Navbar);
->>>>>>> 355fd7ed684f90a18094f0e01bd29e8fc67b93d9
