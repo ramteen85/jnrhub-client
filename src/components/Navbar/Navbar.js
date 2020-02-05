1322ff90d9f4a98ec1cfe6a33f91f925e3f89be1
Kevin Osborn <kevinosborn31@gmail.com>
2020-02-05 15:35:12 +1100

    conflict

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,44 +1,36 @@
 import React, { Component } from 'react';
 import styles from './Navbar.module.css';
 import {Link, withRouter} from 'react-router-dom';
-
 class Navbar extends Component {
-
     state = {
         loggedin: false
     }
-
     constructor(props) {
         super(props);
         this.state.loggedin = this.props.loggedin;
     }
-
     logoutHandler = () => {
         this.loggedin=false
         this.props.history.push('/');
     }
-
     render() {
         return(
-<<<<<<< HEAD
-            <div className={styles.navwrapper}>
-                { this.state.loggedin
+            <div>
+                { this.state.loggedin === true
                 ?
                 <nav className={styles.loggedinnav}>
                     <p className={styles.brand}>&lt;Jnr/Hub&gt;</p>
                     <ul>
-<<<<<<< HEAD
-                        <li><a href="/">Dashboard</a></li>
-                        <li><a href="/">Job Board</a></li>
-                        <li><a href="/">Profile</a></li>
-                        <li><a href="/">Logout</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/">Jobs</Link></li>
+                        <li><Link to="/">Portfolio</Link></li>
+                        <li><Link to="/">Logout</Link></li>
                     </ul>
                 </nav>
                 :
-                <nav>
+                <nav className={styles.loggedoutnav}>
                     <ul>
                         <li>
-
                         </li>
                     </ul>
                 </nav>
@@ -46,11 +38,5 @@ class Navbar extends Component {
             </div>
         )
     }
-
 }
-
-<<<<<<< HEAD
-export default Navbar;
-=======
 export default withRouter(Navbar);
->>>>>>> 355fd7ed684f90a18094f0e01bd29e8fc67b93d9
