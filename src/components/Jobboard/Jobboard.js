63becfd9540e4b3247380703d6f8903501024f34
Ramteen Taheri <ramteen.taheri@gmail.com>
2020-02-06 16:33:54 +1100

    updated jobboard tables with database

--- src/components/Jobboard/Jobboard.js
+++ src/components/Jobboard/Jobboard.js
@@ -5,25 +5,21 @@ import axios from 'axios';
 class Jobboard extends Component {
 
   state = {
-
+    jobs: []
   }
 
-  render() {
-
-    const jobs = axios.get('http://localhost:3000/jobs')
+  componentDidMount() {
+    axios.get('http://localhost:3000/jobs')
     .then(res => {
       console.log(res.data);
-      // res.data.map((job, key) => {
-      //   <tr key={job.id}>
-      //     <td className={styles.jobcolumn}>{job.company_name}</td>
-      //     <td className={styles.jobcolumn}>{job.location}</td>
-      //     <td className={styles.jobcolumn}>{job.role}</td>
-      //   </tr>
-      // });
+      this.setState({ jobs: res.data });
     })
     .catch(err => {
       console.log(err);
     });
+  }
+
+  render() {
 
     return(
       <div className={styles.container}>
@@ -39,48 +35,14 @@ class Jobboard extends Component {
             </tr>
           </thead>
           <tbody>
-          <tr className={styles.job}>
-            <td className={styles.jobcolumn}>Google</td>
-            <td className={styles.jobcolumn}>Sydney</td>
-            <td className={styles.jobcolumn}>Junior Front End</td>
+          { this.state.jobs.map((job, key) => (
+            <tr key={job.id}>
+            <td className={styles.jobcolumn}>{job.company_name}</td>
+            <td className={styles.jobcolumn}>{job.location}</td>
+            {/* <td className={styles.jobcolumn}>{job.role}</td> */}
             <td className={styles.jobcolumn}><a href="#/job">Apply</a></td>
-          </tr>
-          <tr className={styles.job}>
-            <td className={styles.jobcolumn}>Facebook</td>
-            <td className={styles.jobcolumn}>Melbourne</td>
-            <td className={styles.jobcolumn}>Senior Front End</td>
-            <td className={styles.jobcolumn}><a href="#/job">Apply</a></td>
-          </tr>
-          <tr className={styles.job}>
-            <td className={styles.jobcolumn}>Uber</td>
-            <td className={styles.jobcolumn}>New York</td>
-            <td className={styles.jobcolumn}>Graduate Front End</td>
-            <td className={styles.jobcolumn}><a href="#/job">Apply</a></td>
-          </tr>
-          <tr className={styles.job}>
-            <td className={styles.jobcolumn}>General Assembly</td>
-            <td className={styles.jobcolumn}>Sydney</td>
-            <td className={styles.jobcolumn}>Instructor</td>
-            <td className={styles.jobcolumn}><a href="#/job">Apply</a></td>
-          </tr>
-          <tr className={styles.job}>
-            <td className={styles.jobcolumn}>Grumpy Sailor</td>
-            <td className={styles.jobcolumn}>Sydney</td>
-            <td className={styles.jobcolumn}>Creative Developer</td>
-            <td className={styles.jobcolumn}><a href="#/job">Apply</a></td>
-          </tr>
-          <tr className={styles.job}>
-            <td className={styles.jobcolumn}>Blizzard</td>
-            <td className={styles.jobcolumn}>Sydney</td>
-            <td className={styles.jobcolumn}>Game Destroyer (developer)</td>
-            <td className={styles.jobcolumn}><a href="#/job">Apply</a></td>
-          </tr>
-          <tr className={styles.job}>
-            <td className={styles.jobcolumn}>Westpac</td>
-            <td className={styles.jobcolumn}>Sydney</td>
-            <td className={styles.jobcolumn}>Junior Back End</td>
-            <td className={styles.jobcolumn}><a href="#/job">Apply</a></td>
-          </tr>
+            </tr>
+          ))}
           </tbody>
         </table>
       </div>
