4391c0122ba4d48dc42ec0d5492b0e5dd69c710b
Ramteen Taheri <ramteen.taheri@gmail.com>
2020-02-06 12:55:34 +1100

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Lobbyemp from './components/Lobbyemp/Lobbyemp';
 import Lobbyjs from './components/Lobbyjs/Lobbyjs';
-import Logout from './components/Logout/Logout';
 
 
 import {
@@ -20,25 +19,36 @@ class App extends Component {
     loggedin: false
   }
 
-  logout = () => {
-    this.setState({ loggedin: false });
+  // logout = () => {
+  //   this.setState({ loggedin: false });
+  // }
+
+  // login = () => {
+  //   this.setState({ loggedin: true });
+  // }
+
+  setLoginStatus = (loggedIn) => {
+    this.setState({ loggedin: loggedIn });
   }
 
-  login = () => {
-    this.setState({ loggedin: true });
+  performLogout = () => {
+    this.setState({ loggedin: false });
+    localStorage.removeItem('jwt');
   }
 
   render() {
     return (
       <Router>
         <div className="App">
-            <Navbar loggedin={this.state.loggedin} logout={this.logout} login={this.login} />
+            <Route path="/" render={(props) => <Navbar {...props} loggedin={this.state.loggedin} onLogout={this.performLogout}  />} />
             <Switch>
-              <Route path="/" exact component={Login} />
+              <Route path="/" exact render={ (props) => <Login {...props} onLogin={this.setLoginStatus} /> } />
               <Route path="/register" exact component={Register} />
               <Route path="/jobseeker" exact component={Lobbyjs} />
               <Route path="/employer" exact component={Lobbyemp} />
-              <Route path="/logout" exact component={Logout} />
+              {
+              // <Route path="/logout" exact component={Logout} />
+              }
             </Switch>
         </div>
       </Router>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import styles from './Navbar.module.css';
 import {  Link } from 'react-router-dom';
 class Navbar extends Component {
-    state = {
-        loggedin: false
+    // state = {
+    //     loggedin: true
+    // }
+
+    handleLogout = () => {
+        this.props.onLogout();
+        this.props.history.push( '/' );
     }
 
     render() {
@@ -13,14 +18,15 @@ class Navbar extends Component {
                     <p className={styles.brand}>&lt;Jnr/Hub&gt;</p>
                     <ul>
                         <li><Link to="/">Home</Link></li>
-                        { this.state.loggedin
-                        ?
+                        { this.props.loggedin
+                        ?   
                         <div className={styles.inline}>
                             <li><Link to="/">Job Board</Link></li>
-                            <li><Link to="/logout" onClick={() => this.setState({ loggedin: false })}>Logout</Link></li>
+                            <li><button className={styles.oddOneOut} onClick={ this.handleLogout }>Logout</button></li>
                         </div>
                         :
                         <li><Link to="/register">Register</Link></li>
+                        //  onClick={() => this.setState({ loggedin: false })}
                         }
                     </ul>
                 </nav>
