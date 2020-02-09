33e1c89bae876d88792aa01d8dfefa5fa8317c84
Kevin Osborn <kevinosborn31@gmail.com>
2020-02-09 21:32:07 +1100

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-# Jnr-Hub Front End
-
-* Used React with Ruby on Rails back end
-* Created by Ramteen Taheri, Kevin Osborn, and Oliver Cook
-* [Live Front End](https://ramteen85.github.io/jnrhub-client/#/)
-* [Live Back End](https://whispering-chamber-55079.herokuapp.com/)
---
+++ precache-manifest.30861f9970c867b1a2ecd9ff64bfac4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e75ad56338ec79544b83ac7eabbc73",
+    "url": "/jnrhub-client/index.html"
+  },
+  {
+    "revision": "d4e4cced0fae416dee1a",
+    "url": "/jnrhub-client/static/css/main.d61c2647.chunk.css"
+  },
+  {
+    "revision": "cca04ac1a7ab63becf7d",
+    "url": "/jnrhub-client/static/js/2.aff1a717.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/jnrhub-client/static/js/2.aff1a717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e4cced0fae416dee1a",
+    "url": "/jnrhub-client/static/js/main.f5d83cbe.chunk.js"
+  },
+  {
+    "revision": "eaa4a69a49b5f36d54d4",
+    "url": "/jnrhub-client/static/js/runtime-main.1e61fd59.js"
+  }
+]);
